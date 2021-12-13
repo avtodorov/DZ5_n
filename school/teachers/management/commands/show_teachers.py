9202377ad3b3fc726c9e2beb69538ee51801c5bb
andrey <a4412308@gmail.com>
2021-12-13 16:51:02 +0200

    correction

--- school/teachers/management/commands/show_teachers.py
+++ school/teachers/management/commands/show_teachers.py
@@ -1,23 +1,37 @@
-
 from django.core.management import BaseCommand
-
+from django.db.models import Q
 
 from teachers.models import Teacher
 
 
 class Command(BaseCommand):
+
     def add_arguments(self, parser):
-        parser.add_argument('choice', type=str, default='first_name', nargs='?', help='Choices are: first_name, '
-                                                                                      'last_name, theme')
-        parser.add_argument('name', type=str, nargs='?', help='Give a parameter name to search in data')
+        parser.add_argument('-f', '--first_name', nargs='?', type=str, help='Lookup field first_name')
+        parser.add_argument('-l', '--last_name', nargs='?', type=str, help='Lookup field last_name')
+        parser.add_argument('-t', '--theme', nargs='?', type=str, help='Lookup field theme')
 
-    def handle(self, choice, name, **options):
+    def handle(self, *args, **options):
 
-        if choice == 'last_name':
-            data = Teacher.objects.filter(last_name=name)
-        elif choice == 'theme':
-            data = Teacher.objects.filter(theme=name)
+        first_name = options['first_name']
+        last_name = options['last_name']
+        theme = options['theme']
+
+        if first_name and last_name and theme:
+            data = Teacher.objects.filter(Q(theme=theme) & Q(first_name__contains=first_name) & Q(last_name__contains=last_name))
+        elif theme and first_name:
+            data = Teacher.objects.filter(Q(theme=theme) & Q(first_name__contains=first_name))
+        elif theme and last_name:
+            data = Teacher.objects.filter(Q(theme=theme) & Q(last_name__contains=last_name))
+        elif first_name and last_name:
+            data = Teacher.objects.filter(Q(first_name__contains=first_name) & Q(last_name__contains=last_name))
+        elif theme:
+            data = Teacher.objects.filter(Q(theme=theme))
+        elif first_name:
+            data = Teacher.objects.filter(Q(first_name__contains=first_name))
+        elif last_name:
+            data = Teacher.objects.filter(Q(last_name__contains=last_name))
         else:
-            data = Teacher.objects.filter(first_name=name)
+            data = Teacher.objects.all()
 
-        print(f'Teachers with choice:{choice} with name:{name} : {data}')
+        print(f'Found teachers :{data}')
